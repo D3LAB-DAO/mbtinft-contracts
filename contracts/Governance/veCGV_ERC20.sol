320d9667cfc8591282366327e406e4c533c41ad2
lukepark <lukepark327@gmail.com>
2022-02-21 20:50:44 +0900

    veCGV for voting

---
+++ contracts/Governance/veCGV_ERC20.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20VotesComp.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract veCGV is ERC20VotesComp, Ownable {
+    constructor() ERC20("vote-escrowed CGV", "veCGV") ERC20Permit("vote-escrowed CGV") {}
+    
+    /**
+     * @notice Stake CGV for veCGV.
+     *
+     * TODO:
+     *
+     * - Ownable.
+     */
+    function mint(address account, uint256 amount) public {
+        _mint(account, amount);
+    }
+}
