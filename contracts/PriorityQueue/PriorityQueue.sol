051bf151f9cbc6102c37c09b30dbde635a168685
lukepark <lukepark327@gmail.com>
2022-02-21 12:24:25 +0900

    implement Priority Queue

---
+++ contracts/PriorityQueue/PriorityQueue.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Heap.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * Implementation for Solidity v0.8.
+ *
+ * References:
+ *
+ * - https://github.com/zmitton/eth-heap
+ *
+ * TODO:
+ *
+ * - Ownable.
+ */
+contract PriorityQueue is Ownable {
+    using Heap for Heap.Data;
+    Heap.Data public data;
+
+    constructor() {
+        data.init();
+    }
+
+    function heapify(int128[] memory priorities) public {
+        for(uint i ; i < priorities.length ; i++){
+            data.insert(priorities[i]);
+        }
+    }
+    function insert(int128 priority) public returns(Heap.Node memory) {
+        return data.insert(priority);
+    }
+    function extractMax() public returns(Heap.Node memory) {
+        return data.extractMax();
+    }
+    function extractById(int128 id) public returns(Heap.Node memory) {
+        return data.extractById(id);
+    }
+
+    /* view */
+    function dump() public view returns(Heap.Node[] memory) {
+        return data.dump();
+    }
+    function getMax() public view returns(Heap.Node memory) {
+        return data.getMax();
+    }
+    function getById(int128 id) public view returns(Heap.Node memory) {
+        return data.getById(id);
+    }
+    function getByIndex(uint i) public view returns(Heap.Node memory) {
+        return data.getByIndex(i);
+    }
+    function size() public view returns(uint){
+        return data.size();
+    }
+    function idCount() public view returns(int128){
+        return data.idCount;
+    }
+    function indices(int128 id) public view returns(uint){
+        return data.indices[id];
+    }
+}
