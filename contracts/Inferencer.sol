9268c27f1bde58db31581c57e858616d7683126e
lukepark <lukepark327@gmail.com>
2022-02-22 00:54:35 +0900

    Inference.sol -> Inferencer.sol

---
+++ contracts/Inferencer.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "./Talk_ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Inferencer is Ownable {
+    Talk public talk;
+    
+    /* NFT-ize */
+    // talk.mint();
+}
